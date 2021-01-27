c9e078de10487297826b2e91cff86a3a8dd524e4
lovel1 <qwertapbru@gmail.com>
2021-01-27 19:15:19 +0300

    fanikauppa

---
+++ fanikauppa/precache-manifest.77c22dc3dd500b6d2b46a86d04d765d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20db9b9d2591ca81a0d746bf3b2a010",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "4f075a0dcf4b9fbb83c3",
+    "url": "/fanikauppa/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9bb522c7a1e8495f9cbf",
+    "url": "/fanikauppa/static/css/main.22bec680.chunk.css"
+  },
+  {
+    "revision": "4f075a0dcf4b9fbb83c3",
+    "url": "/fanikauppa/static/js/2.e78df97c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fanikauppa/static/js/2.e78df97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb522c7a1e8495f9cbf",
+    "url": "/fanikauppa/static/js/main.605c0194.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  }
+]);
